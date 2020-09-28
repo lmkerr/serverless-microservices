b41d1b82d6df9d7ba25efd1a363eb3da7942e617
Loren Kerr <lkerr@singularity-interactive.com>
2020-09-28 15:28:38 -0600

    closes auctions

--- auction-service/src/handlers/auction/process-auctions.js
+++ auction-service/src/handlers/auction/process-auctions.js
@@ -1,8 +1,19 @@
 import { getEndedAuctions } from "../../lib/get-ended-auctions.helper";
+import { closeAuction } from "../../lib/close-auction";
+import createError from "http-errors";
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
