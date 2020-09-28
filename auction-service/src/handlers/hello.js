717d60d7757fc85c277f6ced5d1bd945042badf0
Loren Kerr <lkerr@singularity-interactive.com>
2020-09-28 09:57:05 -0600

    dynamodb added

--- auction-service/src/handlers/hello.js
+++
@@ -1,8 +0,0 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from https://lorenkerr.com" }),
-  };
-}
-
-export const handler = hello;
