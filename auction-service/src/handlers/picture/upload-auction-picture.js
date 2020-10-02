de917dbed2171c2e2e7b9d5f29b6ef0aa916073d
Loren M Kerr <lorenmkerr@Lorens-MacBook-Pro.local>
2020-10-02 13:21:44 -0600

    cors

--- auction-service/src/handlers/picture/upload-auction-picture.js
+++ auction-service/src/handlers/picture/upload-auction-picture.js
@@ -1,6 +1,7 @@
 import middy from "@middy/core";
 import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
+import cors from "@middy/http-cors";
 import validator from "@middy/validator";
 
 import { getAuctionById } from "../auction/get-auction";
@@ -39,4 +40,5 @@ export async function uploadAuctionPicture(event) {
 
 export const handler = middy(uploadAuctionPicture)
   .use(httpErrorHandler())
-  .use(validator({ inputSchema: uploadAuctionPictureSchema }));
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }))
+  .use(cors());
